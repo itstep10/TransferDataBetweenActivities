6f58aac8bc668cb8f4fbc8bbf0dd2a3c94545333
Vitalii Koren <Koren_V@itstep.org>
2018-05-12 01:49:19 +0300

    init

---
+++ app/src/main/java/org/itstep/transferdatabetweenactivities/MainActivity.java
@@ -0,0 +1,97 @@
+package org.itstep.transferdatabetweenactivities;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    public static final String TAG_MESSAG = "org.itstep.transferdatabetweenactivities.MainActivity.message";
+
+    private static final String TAG = "DebugMainActivity";
+    private static final int ACTIVITY_REQUEST_CODE = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.b_go_to_next_activity);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(TAG_MESSAG, "Hi! I came from previe Activity");
+                startActivity(intent);
+                startActivityForResult(intent, ACTIVITY_REQUEST_CODE);
+            }
+        });
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == ACTIVITY_REQUEST_CODE)
+        {
+            String message = "Empty result";
+            if(data.hasExtra(SecondActivity.MESSAGE_RESULT))
+                message = data.getStringExtra(SecondActivity.MESSAGE_RESULT);
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this, "Wrong result", Toast.LENGTH_SHORT).show();
+    }
+}
+
---
+++ app/src/main/java/org/itstep/transferdatabetweenactivities/SecondActivity.java
@@ -0,0 +1,87 @@
+package org.itstep.transferdatabetweenactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity
+{
+    public static final String MESSAGE_RESULT = "org.itstep.transferdatabetweenactivities.SecondActivity.message_result";
+    private static final String TAG = "DebugSecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        if(intent != null)
+        {
+            String message = "Empty result";
+            if(intent.hasExtra(MainActivity.TAG_MESSAG))
+                message = intent.getStringExtra(MainActivity.TAG_MESSAG);
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        setContentView(R.layout.activity_second);
+        Button button = findViewById(R.id.b_back);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.putExtra(MESSAGE_RESULT, "Hi! I'm from Second Activity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+}
